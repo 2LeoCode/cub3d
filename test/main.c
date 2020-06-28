4bdb964a3146793f822f66c67cf4b43077df3ff8
BuildTools <unconfigured@null.spigotmc.org>
2020-06-28 18:07:06 +0200

    suicidolino

--- test/main.c
+++ test/main.c
@@ -148,7 +148,8 @@ void	putline(t_mlxvar mlx, t_coord A, t_coord B)
 	while (++j <= ABS(A.Y - B.Y))
 	{
 		i = -1;
-		while (++i <= (int)ABS(A.X - B.X))
+		printf("here2\n");
+		while (++i <= ABS(A.X - B.X))
 			if (!pasX || !pasY || ((int)(i * pasY + (A.Y - startY)) == j)){printf("here\n");
 				mlx_pixel_put(mlx.key, mlx.win, i + startX, j + startY, 255);}
 	}
