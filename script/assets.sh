a630f3a0c0927757ea21278de8e7a03371722743
BuildTools <unconfigured@null.spigotmc.org>
2020-08-07 18:53:24 +0200

    suicido

--- script/assets.sh
+++ script/assets.sh
@@ -2,4 +2,6 @@
 
 mkdir assets
 cd assets > /dev/null 2>&1 
-wget https://lodev.org/cgtutor/files/wolftex.zip > /dev/null 2>&1 
+wget https://lodev.org/cgtutor/files/wolftex.zip > /dev/null 2>&1
+tar -xf wolftex.zip
+cd ..
