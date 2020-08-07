7cf4a6c068e3a7745e80c7684b11313bbbe71a9e
BuildTools <unconfigured@null.spigotmc.org>
2020-08-07 18:34:12 +0200

    suicido

--- src/cub3d.c
+++ src/cub3d.c
@@ -12,25 +12,6 @@
 
 #include <cub3d.h>
 
-void	init_nrm(t_mlxvar *cub3d)
-{
-	cub3d->nrm_X.X = 1;
-	cub3d->nrm_X.Y = 0;
-	cub3d->nrm_X.Z = 0;
-	cub3d->nrm_Y.X = 0;
-	cub3d->nrm_Y.Y = 1;
-	cub3d->nrm_Y.Z = 0;
-	cub3d->nrm_Z.X = 0;
-	cub3d->nrm_Z.Y = 0;
-	cub3d->nrm_Z.Z = -1;
-}
-
-void	refresh_nrm(t_mlxvar *cub3d)
-{
-	cub3d->look.X = sin(cub3d->set->rot_hor);
-	cub3d->look.Y = sin(cub3d->set->rot_vert);
-	cub3d->look.Z = look.X * tan((M_PI / 2) - cub3d->set->rot_hor);
-}
 
 int		cub3D(t_set *set, int flags)
 {
@@ -41,17 +22,6 @@ int		cub3D(t_set *set, int flags)
 	save = false;
 	if ((flags == SAVE) || (flags == SAVE | BONUS))
 		save = true;
-	if (((flags == BONUS) || (flags == SAVE | BONUS)))
-	{
-		if ((check = check_set_bonus(&set)) != 0)
-			return (error_wrong_map(check));
-		return (cub3D_bonus(set, save));
-	}
-	cub3d.set = set;
-	init_nrm(&cub3d)
-	refresh_pos(cub3d);
-	if (!(cub3d.key = mlx_init()) || !(cub3d.win = mlx_new_window(cub3d.key, set->X, set->Y, "Cub3D")))
-		return (error_mlx(ER_DEFLT));
-	clear_set(set);
+	display_map(set->map);
 	return (0);
 }
