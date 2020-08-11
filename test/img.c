1520e6fd468e1fc6497486869908b40148f05225
BuildTools <unconfigured@null.spigotmc.org>
2020-08-12 01:28:30 +0200

    sui

--- test/img.c
+++ test/img.c
@@ -34,7 +34,7 @@ int		main(void)
 	void	*win;
 
 	id = mlx_init();
-	win = mlx_new_window(id, 500, 500);
+	win = mlx_new_window(id, 500, 500, "test");
 	img.surface = mlx_xpm_file_to_image(id, "../assets/2d_floor.xpm", &img.w, &img.h);
 	img.data = (int*)mlx_get_data_addr(img.surface, &img.bpp, &img.linesiz, &img.endian);
 	mlx_put_image_to_window (id, win, img.surface, 10, 10);
