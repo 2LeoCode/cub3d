49469188ecbb735083ffbe5beee3c67dfc25bcd6
BuildTools <unconfigured@null.spigotmc.org>
2020-08-30 18:10:02 +0200

    sui

--- src/rays.c
+++ src/rays.c
@@ -34,11 +34,13 @@ t_ray	*update_rays(t_mlxvar mlxvar)
 		dy = sin(r) / 50;
 		bx = mlxvar.posX;
 		by = mlxvar.posY;
+		printf("%lf\n%lf\n", dx, dy);
 		while (mlxvar.set->map[(int)by][(int)bx] == '0')
 		{
 			bx += dx;
 			by += dy;
 		}
+		printf("2\n");
 		tmp[i].siz = sqrt((bx - mlxvar.posX) * (bx - mlxvar.posX) + (by - mlxvar.posY) * (by - mlxvar.posY));
 		if (mlxvar.set->map[(int)by][(int)(bx - dx)] == '0')
 		{
