96304607cf57d69a776b370e734bac7f1695a6f0
BuildTools <unconfigured@null.spigotmc.org>
2020-09-02 19:42:10 +0200

    sui

--- src/rays.c
+++ src/rays.c
@@ -40,7 +40,7 @@ t_ray	*update_rays(t_mlxvar mlxvar)
 			by += dy;
 		}
 		tmp[i].siz = sqrt((bx - mlxvar.posX) * (bx - mlxvar.posX) + (by - mlxvar.posY) * (by - mlxvar.posY));
-		if ((double)(bx - (int)bx) < 0.001)
+		if ((double)(bx - dx) < (double)((int)bx + 0.001) && (double)(bx - dx) > (double)((int)bx - 0.001))
 		{
 			tmp[i].texture = (((mlxvar.set->rot_hor - r > M_PI) && (mlxvar.set->rot_hor - r < 3 * M_PI / 4)) ? &mlxvar.wallW : &mlxvar.wallE);
 			tmp[i].col_pos = (by - (int)by) * tmp[i].texture->width;
