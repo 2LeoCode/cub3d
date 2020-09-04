3763c50a3534ff94e41c12e85cd1ff8945e02b4a
BuildTools <unconfigured@null.spigotmc.org>
2020-09-04 18:09:21 +0200

    sui

--- src/rays.c
+++ src/rays.c
@@ -36,7 +36,7 @@ t_ray	*update_rays(t_mlxvar mlxvar)
 		else
 			b.y = (int)mlxvar.posY - 0.0001;
 		b.x = mlxvar.posX + ft_dabs(b.y - mlxvar.posY) * tan(mlxvar.set->rot_hor + r);
-			printf("%lf %lf %lf\n", b.x, b.y, d);
+		printf("%lf %lf %lf\n", b.x, b.y, mlxvar.set->rot_hor + r);
 		while (mlxvar.set->map[(int)b.y][(int)b.x] - '1')
 		{
 			b.x += d;
