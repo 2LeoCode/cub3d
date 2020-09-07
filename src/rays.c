143b39ce5704f6b44876a8b67f259deff8091982
BuildTools <unconfigured@null.spigotmc.org>
2020-09-07 20:02:20 +0200

    sui

--- src/rays.c
+++ src/rays.c
@@ -30,46 +30,50 @@ int		update_rays(t_mlxvar *mlxvar)
 	{
 		a = mlxvar->set->rot_hor + r;
 		if (a < 0)
-			a = 2 * M_PI + a;
-		if (a > (2 * M_PI))
-			a = 2 * M_PI - a;
+			a = 2 * M_PI + ((a > (2 * M_PI)) ? a : -a);
 		mlxvar->rays[i].rot = r;
-		d = tan(mlxvar->set->rot_hor + r);
-		if (a < M_PI)
+		d = 1 / (tan(a) + 0.0001);
+		if (a <= M_PI)
 			b.y = (int)mlxvar->posY + 1.0001;
-		else if (!a || (mlxvar->set->rot_hor + r) == M_PI)
+		else if (!a || a == M_PI)
 			b.y = mlxvar->posY;
 		else
 			b.y = (int)mlxvar->posY - 0.0001;
-		b.x = mlxvar->posX + ft_dabs(b.y - mlxvar->posY) * d;
+		b.x = mlxvar->posX + (-((a > M_PI / 2) && (a < 3 * M_PI / 2)) * 2 + 1) * ft_dabs(b.y - mlxvar->posY) * d;
 		while ((b.x > 0) && (b.y > 0) && (b.x < mlxvar->set->mapX) && (b.y < mlxvar->set->mapY) && (mlxvar->set->map[(int)c.y][(int)c.x] - '1'))
 		{
 			b.x += d;
-			b.y++;
+			if (a < M_PI)
+				b.y++;
+			else if ((a > M_PI) && (a < M_PI / 2))
+				b.y--;
 		}
 		length.y = sqrt((b.x - mlxvar->posX) * (b.x - mlxvar->posX) + (b.y - mlxvar->posY) * (b.y - mlxvar->posY));
 		d = 1 / d;
-		if (((mlxvar->set->rot_hor + r) > (M_PI / 2)) && ((mlxvar->set->rot_hor + r) < (3 * M_PI / 4)))
-			c.x = (int)mlxvar->posX + 1.0001;
-		else if ((mlxvar->set->rot_hor + r) == (M_PI / 2) || (mlxvar->set->rot_hor + r) == (3 * M_PI / 4))
-			c.x = (int)mlxvar->posX - 0.0001;
-		c.y = mlxvar->posY + ft_dabs(c.x - mlxvar->posX) * d;
+		if (a <= M_PI / 2)
+			b.y = (int)mlxvar->posY + 1.0001;
+		else
+			b.y = (int)mlxvar->posY - 0.0001;
+		c.y = mlxvar->posY + (-(a > M_PI) * 2 + 1) * ft_dabs(c.x - mlxvar->posX) * d;
 		while ((b.x > 0) && (b.y > 0) && (b.x < mlxvar->set->mapX) && (b.y < mlxvar->set->mapY) && (mlxvar->set->map[(int)c.y][(int)c.x] - '1'))
 		{
 			c.y += d;
-			c.x++;
+			if (a < (M_PI / 2) || a > (3 * M_PI / 2))
+				c.x++;
+			else if (a > (M_PI / 2) && a < (3 * M_PI / 2))
+				c.x--;
 		}
 		length.x = sqrt((c.x - mlxvar->posX) * (c.x - mlxvar->posX) + (c.y - mlxvar->posY) * (c.y - mlxvar->posY));
 		if (length.y < length.x)
 		{
 			mlxvar->rays[i].siz = length.y;
-			mlxvar->rays[i].texture = (((mlxvar->set->rot_hor + r > 0) && (mlxvar->set->rot_hor + r < M_PI)) ? &mlxvar->wallS : &mlxvar->wallN);
+			mlxvar->rays[i].texture = ((a < M_PI) ? &mlxvar->wallS : &mlxvar->wallN);
 			mlxvar->rays[i].col_pos = (double)(b.x - (int)b.x) * mlxvar->rays[i].texture->width;
 		}
 		else
 		{
 			mlxvar->rays[i].siz = length.x;
-			mlxvar->rays[i].texture = (((mlxvar->set->rot_hor + r > (M_PI / 2)) && (mlxvar->set->rot_hor + r < (3 * M_PI / 4))) ? &mlxvar->wallW : &mlxvar->wallE);
+			mlxvar->rays[i].texture = (((a > (M_PI / 2)) && (a < (3 * M_PI / 4))) ? &mlxvar->wallW : &mlxvar->wallE);
 			mlxvar->rays[i].col_pos = (double)(c.y - (int)c.y) * mlxvar->rays[i].texture->width;
 		}
 		r += (mlxvar->set->FOV / mlxvar->set->X);
