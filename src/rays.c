90375eebb1c1d86ff9fb84652ebd083ed5949704
BuildTools <unconfigured@null.spigotmc.org>
2020-09-08 20:50:17 +0200

    sui

--- src/rays.c
+++ src/rays.c
@@ -17,10 +17,11 @@ int		update_rays(t_mlxvar *mlxvar)
 	int			i;
 	double		r;
 	double		a;
+	double		t;
 	t_point		b;
 	t_point		c;
 	t_point		length;
-	double		d;
+	t_point		d;
 
 	if (!mlxvar->rays && !(mlxvar->rays = malloc(sizeof(t_ray) * mlxvar->set->X)))
 		return (-1);
@@ -31,42 +32,50 @@ int		update_rays(t_mlxvar *mlxvar)
 	{
 		a = mlxvar->set->rot_hor + r;
 		if (a < 0)
-			a = 2 * M_PI + ((a > (2 * M_PI)) ? a : -a);
+			a = 2 * M_PI + a;
+		if (a >= (2 * M_PI))
+			a = 2 * M_PI - a;
 		mlxvar->rays[i].rot = r;
-		d = 1 / (tan(a) + 0.0001);
-		if (a > M_PI / 2 && a < (3 * M_PI / 2))
-			d = -d;
-		if (a <= M_PI)
-			b.y = (int)mlxvar->posY + 1.0001;
+		t = -1 / (tan(a) + (0.0001 * (!a || a == M_PI)));
+		if (a < M_PI)
+		{
+			b.y = (int)mlxvar->posX + 1.0001;
+			d.y = 1;
+		}
 		else if (!a || a == M_PI)
+		{
 			b.y = mlxvar->posY;
+			d.y = -1;
+		}
 		else
 			b.y = (int)mlxvar->posY - 0.0001;
-		b.x = mlxvar->posX + ft_dabs(b.y - mlxvar->posY) * d;
+		b.x = mlxvar->posX + (mlxvar->posY - b.y) * t;
+		d.x = -d.y * t;
 		while ((b.x > 0) && (b.y > 0) && (b.x < mlxvar->set->mapX) && (b.y < mlxvar->set->mapY) && (mlxvar->set->map[(int)b.y][(int)b.x] - '1'))
 		{
-			b.x += d;
-			if (a < M_PI)
-				b.y++;
-			else if ((a > M_PI) && (a < 2 * M_PI))
-				b.y--;
+			b.x += d.x;
+			b.y += d.y;
 		}
 		length.y = sqrt((b.x - mlxvar->posX) * (b.x - mlxvar->posX) + (b.y - mlxvar->posY) * (b.y - mlxvar->posY));
-		d = -1 / d;
-		if (a < M_PI)
-			c.y = (int)mlxvar->posY + 1.0001;
-		else if (!a || a == M_PI)
-			c.y = mlxvar->posY;
+		t = -tan(a);
+		if (a < (M_PI / 2) || a > (3 * M_PI / 2))
+		{
+			c.x = (int)mlxvar->posX + 1.0001;
+			d.x = 1;
+		}
+		else if (a == (M_PI / 2) || a == (3 * M_PI / 2))
+			c.x = mlxvar->posY;
 		else
-			c.y = (int)mlxvar->posY - 0.0001;
-		c.x = mlxvar->posY + ft_dabs(c.x - mlxvar->posX) * d;
+		{
+			c.x = (int)mlxvar->posY - 0.0001;
+			d.x = -1;
+		}
+		c.y = mlxvar->posY + (mlxvar->posX - c.x) * t;
+		d.y = -d.x * t;
 		while ((c.x > 0) && (c.y > 0) && (c.x < mlxvar->set->mapX) && (c.y < mlxvar->set->mapY) && (mlxvar->set->map[(int)c.y][(int)c.x] - '1'))
 		{
-			c.y += d;
-			if (a < (M_PI / 2) || a > (3 * M_PI / 2))
-				c.x++;
-			else if (a > (M_PI / 2) && a < (3 * M_PI / 2))
-				c.x--;
+			c.y += d.y;
+			c.x += d.x
 		}
 		length.x = sqrt((c.x - mlxvar->posX) * (c.x - mlxvar->posX) + (c.y - mlxvar->posY) * (c.y - mlxvar->posY));
 		if (length.y < length.x)
