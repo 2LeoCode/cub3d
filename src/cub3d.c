103d2c9f9b23007ad1a8e56b15974de47aa42141
BuildTools <unconfigured@null.spigotmc.org>
2020-09-08 23:38:46 +0200

    sui

--- src/cub3d.c
+++ src/cub3d.c
@@ -39,6 +39,7 @@ int		update_screen(t_mlxvar *mlxvar)
 		j = -1;
 		l = (size > mlxvar->screen.height) ? 0 : (int)(mlxvar->screen.height / 2) - (size / 2);
 		k = (size < mlxvar->screen.height) ? 0 : ((size / 2) - (mlxvar->screen.height / 2) / size) * mlxvar->rays[i].texture->height;
+		printf("%f\n", k);
 		while (++j < (((int)size > mlxvar->screen.height) ? mlxvar->screen.height : (int)size))
 		{
 			mlxvar->screen.img_data[(l + j) * mlxvar->screen.width + i] = mlxvar->rays[i].texture->img_data[(int)k * mlxvar->rays[i].texture->width + mlxvar->rays[i].col_pos];
