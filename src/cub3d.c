2e8b5dd06869b587e0f6587d25dfa77139315b1f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-08 16:55:30 +0200

    sui

--- src/cub3d.c
+++ src/cub3d.c
@@ -229,6 +229,8 @@ int		draw_sprites(t_mlxvar *mlx)
 	while (lst)
 	{
 		a = lst->a - mlx->set->rot_hor;
+		if (mlx->set->rot_hor > _3PI2)
+			a = -a - _2PI;
 		size = (double)mlx->screen.height / (cos(a) * lst->len);
 		screenC.X = ((a + (mlx->set->FOV / 2) / mlx->set->FOV) * mlx->screen.width) - (size / 2);
 		printf("lst->a: %lf, rot_hor: %lf, a: %lf, screenX: %d\n", lst->a, mlx->set->rot_hor, a, screenC.X);
