6a05fe387c6b64a23b00c42aa91cabed4c58383c
2LeoCode <leo.13suardi@outlook.com>
2020-10-10 21:54:36 +0200

    sui

--- src/clear.c
+++ src/clear.c
@@ -6,7 +6,7 @@
 /*   By: lsuardi <lsuardi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 22:43:31 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/10/10 15:13:48 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/10/10 21:54:14 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,21 @@ void		clear_set(t_set *set)
 	int		i;
 
 	i = -1;
-	if (set->NO)
-		free(set->NO);
-	set->NO = NULL;
-	if (set->WE)
-		free(set->WE);
-	set->WE = NULL;
-	if (set->EA)
-		free(set->EA);
-	set->EA = NULL;
-	if (set->SO)
-		free(set->SO);
-	set->SO = NULL;
-	if (set->S)
-		free(set->S);
-	set->S = NULL;
+	if (set->no)
+		free(set->no);
+	set->no = NULL;
+	if (set->we)
+		free(set->we);
+	set->we = NULL;
+	if (set->ea)
+		free(set->ea);
+	set->ea = NULL;
+	if (set->so)
+		free(set->so);
+	set->so = NULL;
+	if (set->s)
+		free(set->s);
+	set->s = NULL;
 	while (set->map && set->map[++i])
 	{
 		free(set->map[i]);
@@ -43,16 +43,16 @@ void		clear_set(t_set *set)
 
 void		clear_paths(t_set *set)
 {
-	free(set->NO);
-	set->NO = NULL;
-	free(set->WE);
-	set->WE = NULL;
-	free(set->EA);
-	set->EA = NULL;
-	free(set->SO);
-	set->SO = NULL;
-	free(set->S);
-	set->S = NULL;
+	free(set->no);
+	set->no = NULL;
+	free(set->we);
+	set->we = NULL;
+	free(set->ea);
+	set->ea = NULL;
+	free(set->so);
+	set->so = NULL;
+	free(set->s);
+	set->s = NULL;
 }
 
 int			clear_mlx_err(t_mlxvar *mlx)
@@ -63,23 +63,23 @@ int			clear_mlx_err(t_mlxvar *mlx)
 
 static int	clear_mlx2(t_mlxvar *mlx)
 {
-	if (mlx->wallE.img_data)
-		free(mlx->wallE.img_data);
-	mlx->wallE.img_data = NULL;
-	if (mlx->wallS.img)
-		mlx_destroy_image(mlx->id, mlx->wallS.img);
-	mlx->wallS.img = NULL;
-	if (mlx->wallS.img_data)
-		free(mlx->wallS.img_data);
-	mlx->wallS.img_data = NULL;
-	if (mlx->wallW.img)
-		mlx_destroy_image(mlx->id, mlx->wallW.img);
-	mlx->wallW.img = NULL;
-	if (mlx->wallW.img_data)
-		free(mlx->wallW.img_data);
-	mlx->wallW.img_data = NULL;
+	if (mlx->walle.img_data)
+		free(mlx->walle.img_data);
+	mlx->walle.img_data = NULL;
+	if (mlx->walls.img)
+		mlx_destroy_image(mlx->id, mlx->walls.img);
+	mlx->walls.img = NULL;
+	if (mlx->walls.img_data)
+		free(mlx->walls.img_data);
+	mlx->walls.img_data = NULL;
+	if (mlx->wallw.img)
+		mlx_destroy_image(mlx->id, mlx->wallw.img);
+	mlx->wallw.img = NULL;
+	if (mlx->wallw.img_data)
+		free(mlx->wallw.img_data);
+	mlx->wallw.img_data = NULL;
 	clear_set(mlx->set);
-	exit(EXIT_SUCCESS);
+	exit(exit_success);
 	return (0);
 }
 
@@ -97,14 +97,14 @@ int			clear_mlx(t_mlxvar *mlx)
 	if (mlx->screen.img_data)
 		free(mlx->screen.img_data);
 	mlx->screen.img_data = NULL;
-	if (mlx->wallN.img)
-		mlx_destroy_image(mlx->id, mlx->wallN.img);
-	mlx->wallN.img = NULL;
-	if (mlx->wallN.img_data)
-		free(mlx->wallN.img_data);
-	mlx->wallN.img_data = NULL;
-	if (mlx->wallE.img)
-		mlx_destroy_image(mlx->id, mlx->wallE.img);
-	mlx->wallE.img = NULL;]
+	if (mlx->walln.img)
+		mlx_destroy_image(mlx->id, mlx->walln.img);
+	mlx->walln.img = NULL;
+	if (mlx->walln.img_data)
+		free(mlx->walln.img_data);
+	mlx->walln.img_data = NULL;
+	if (mlx->walle.img)
+		mlx_destroy_image(mlx->id, mlx->walle.img);
+	mlx->walle.img = NULL;]
 	return (clear_mlx2(mlx));
 }
