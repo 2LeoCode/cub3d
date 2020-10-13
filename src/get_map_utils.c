363b3b468fd61f702c0a766c5824a88a358136eb
BuildTools <unconfigured@null.spigotmc.org>
2020-10-13 15:29:46 +0200

    sui

--- src/get_map_utils.c
+++ src/get_map_utils.c
@@ -100,9 +100,11 @@ int				check_map(t_set *set)
 	tmp = set->map;
 	i = -1;
 	while (tmp[++i])
+		printf("%s\n", tmp[i]);
+	i = -1;
+	while (tmp[++i])
 		if ((ret = workcase(tmp, i, &check, set)))
 			return (ret);
-	printf("OK\n");
 	if (!check)
 	{
 		clear_set(set);
